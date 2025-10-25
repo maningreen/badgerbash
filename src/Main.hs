a3d61b5e438df99f713df84ee6a573aecdadfdfa
maningreen <flibbers@protonmail.com>
2025-10-25 13:10:13 -0600

    basic file setup

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,15 @@
 module Main (main) where
+import GHC.IO.Handle (hGetContents)
+import GHC.IO.Handle.FD (openFile)
+import GHC.IO.IOMode (IOMode(ReadMode))
+
+type WordBank = [String]
+
+wordBankPath :: FilePath
+wordBankPath = "wordbank"
+
+loadWordBank :: IO WordBank
+loadWordBank = openFile wordBankPath ReadMode >>= fmap lines . hGetContents
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = loadWordBank >>= print 
