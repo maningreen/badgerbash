cba04d48b7fa9d66c4c7417cf49fa33976355c66
maningreen <flibbers@protonmail.com>
2025-10-26 14:48:01 -0600

    very simple ui main

--- src/Main.hs
+++ src/Main.hs
@@ -2,10 +2,26 @@ module Main (main) where
 
 import System.Random (getStdGen)
 import Types.WordBank
+import Brick (App(..), str, halt, neverShowCursor, attrMap, fg, attrName, defaultMain)
+import Graphics.Vty (defAttr, white)
+import Brick.Widgets.Border (border)
+import Brick.Widgets.Center (center)
+
+app :: App () () ()
+app = App {
+    appDraw = const . return . center . border $ str "cool"
+  , appChooseCursor = neverShowCursor
+  , appHandleEvent = const halt
+  , appStartEvent = return ()
+  , appAttrMap = const $ attrMap defAttr [
+      (attrName "default", fg white)
+    ]
+  }
 
 main :: IO ()
 main = do
   x <- readWordBank wordBankPath
   gen <- getStdGen
   print $ pickRandomItemWeighted gen x
+  defaultMain app ()
   return ()
