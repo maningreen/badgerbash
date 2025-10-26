a1fa7e75fecaadfba23852d1ac66e8eff205fb28
maningreen <flibbers@protonmail.com>
2025-10-26 12:05:41 -0600

    it can now pick a random (weighted) item

--- src/Main.hs
+++ src/Main.hs
@@ -2,12 +2,12 @@ module Main (main) where
 
 import Data.List ((!?))
 import Data.Maybe (mapMaybe)
+import qualified Data.Vector as V
 import GHC.IO.Handle (hGetContents)
 import GHC.IO.Handle.FD (openFile)
 import GHC.IO.IOMode (IOMode (ReadMode))
-import qualified Data.Vector as V
+import System.Random (Random (randomR), StdGen, getStdGen)
 import Text.Read (readMaybe)
-import System.Random (StdGen, Random (randomR), getStdGen)
 
 data WordItem = WordItem
   { _word :: String
@@ -45,20 +45,28 @@ parseWordBank x = WordBank items total
   items = V.fromList . mapMaybe parseWordMaybe $ lines x
   total = sum . V.map _weight $ items
 
+getItemFromWeight :: Float -> WordBank -> Maybe WordItem
+getItemFromWeight tauWeight bank = snd chosen
+ where
+  chosen = foldFind f 0 items 
+  f :: Float -> WordItem -> (Float, Bool)
+  f sigmaWeight (WordItem _ weight) = (sigmaWeight', sigmaWeight' >= tauWeight)
+   where
+    sigmaWeight' = weight + sigmaWeight
+  items = V.toList $ _items bank
+
 readWordBank :: FilePath -> IO WordBank
 readWordBank x = fmap parseWordBank . hGetContents =<< openFile x ReadMode
 
 pickItem :: StdGen -> WordBank -> (WordItem, StdGen)
-pickItem gen bank = (chosenX, next)
-  where
-    maxI = V.length $ _items bank
-    chosenX = (_items bank) V.! chosenI 
-    (chosenI, next) = randomR (0, maxI) gen :: (Int, StdGen)
+pickItem gen bank = case getItemFromWeight chosenWeight bank of
+  Just x -> (x, next)
+ where
+  (chosenWeight, next) = randomR (_weight . V.head . _items $ bank, _totalWeight bank) gen :: (Float, StdGen)
 
 main :: IO ()
 main = do
   x <- readWordBank wordBankPath
-  print x
   gen <- getStdGen
   print $ pickItem gen x
   return ()
