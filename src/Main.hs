0790b0369e6c90060adac9d57e1bd9678974eddf
maningreen <flibbers@protonmail.com>
2025-10-26 14:06:36 -0600

    split up the files.

--- src/Main.hs
+++ src/Main.hs
@@ -1,19 +1,12 @@
 module Main (main) where
 
-import Data.List ((!?))
 import Data.Maybe (mapMaybe)
 import qualified Data.Vector as V
 import GHC.IO.Handle (hGetContents)
 import GHC.IO.Handle.FD (openFile)
 import GHC.IO.IOMode (IOMode (ReadMode))
 import System.Random (Random (randomR), StdGen, getStdGen)
-import Text.Read (readMaybe)
-
-data WordItem = WordItem
-  { _word :: String
-  , _weight :: Float
-  }
-  deriving (Show)
+import Types.WordItem
 
 data WordBank = WordBank
   { _items :: V.Vector WordItem
@@ -31,13 +24,6 @@ foldFind f acc (x : xs)
 
 wordBankPath :: FilePath
 wordBankPath = "wordbank"
-
-parseWordMaybe :: String -> Maybe WordItem
-parseWordMaybe x = WordItem <$> word <*> weight
- where
-  broken = words x
-  word = broken !? 0
-  weight = (broken !? 1) >>= readMaybe
 
 parseWordBank :: String -> WordBank
 parseWordBank x = WordBank items total
