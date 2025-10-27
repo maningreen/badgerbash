25ef0336051099e9ca939ad2cdfee5ce8e010fb8
maningreen <flibbers@protonmail.com>
2025-10-27 17:23:25 -0600

    colours now depending on if the string is correct or not.

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,6 @@
 module Main (main) where
 
-import Brick (App (..), BrickEvent (VtyEvent), EventM, attrMap, attrName, defaultMain, fg, hLimit, halt, modify, neverShowCursor, str, strWrap, vLimit, txt)
+import Brick (App (..), BrickEvent (VtyEvent), EventM, attrMap, attrName, defaultMain, fg, hLimit, halt, modify, neverShowCursor, strWrap, vLimit, attrName, AttrName, str)
 import Brick.Widgets.Center (center)
 import Control.Monad (void)
 import Graphics.Vty (Event (EvKey), Key (KBS, KChar), black, defAttr, red, white)
@@ -9,7 +9,7 @@ import Types.WordBank
 import Types.WordItem (WordItem (_word))
 import qualified Data.Text as T
 import Data.Text (Text)
-import Util (textInitSafe)
+import Util (textInitSafe, applyAttrToListW)
 
 data State = State
   { _words :: [String]
@@ -33,13 +33,21 @@ app =
             ]
     }
  where
-  draw s = typedWid s : [wordsWid s]
+  draw s = typedWid : [wordsWid]
    where
-    wordsWid = center . vLimit height . hLimit width . strWrap . unwords . take 500 . _words
+    wordsWid = center . vLimit height . hLimit width . strWrap $ w
      where
       width = 100
       height = 3
-    typedWid = txt . _typed
+    w = unwords . take 100 $ _words s
+    typed = _typed s
+    typedWid = applyAttrToListW (str . return . snd) f zipped
+     where
+      zipped = zip w $ T.unpack typed
+      f :: (Char, Char) -> AttrName
+      f (x, y)
+        | x == y = attrName "typed"
+        | otherwise = attrName "wrong"
   handleEvent :: BrickEvent () () -> EventM () State ()
   handleEvent (VtyEvent (EvKey (KChar c) [])) = modify $ addCharToTyped c
   handleEvent (VtyEvent (EvKey KBS [])) = modify dropLastTyped
