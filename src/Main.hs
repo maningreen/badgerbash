603dd89d414540571d484c9f7a48e06ed966a7a4
maningreen <flibbers@protonmail.com>
2025-10-27 07:47:44 -0600

    added stuff

--- src/Main.hs
+++ src/Main.hs
@@ -1,35 +1,41 @@
 module Main (main) where
 
-import System.Random (getStdGen, StdGen)
+import System.Random (getStdGen)
 import Types.WordBank
-import Brick (App(..), str, halt, neverShowCursor, attrMap, fg, attrName, defaultMain, vBox)
-import Graphics.Vty (defAttr, white)
+import Brick (App(..), halt, neverShowCursor, attrMap, fg, attrName, defaultMain, strWrap, EventM, BrickEvent (VtyEvent))
+import Graphics.Vty (defAttr, white, Key (KChar), Event (EvKey), black, red)
 import Control.Monad (void)
 import Types.WordItem (WordItem(_word))
 
 data State = State {
-    _wordBank :: WordBank
-  , _stdGen :: StdGen
+    _words :: [String]
+  , _typed :: String
   }
 
 app :: App State () ()
 app = App {
     appDraw = draw
   , appChooseCursor = neverShowCursor
-  , appHandleEvent = const halt
+  , appHandleEvent = handleEvent
   , appStartEvent = return ()
   , appAttrMap = const $ attrMap defAttr [
-      (attrName "default", fg white)
+      (attrName "default", fg black)
+    , (attrName "wrong", fg red)
+    , (attrName "typed", fg white)
     ]
   }
   where
-    draw (State x gen) = return . vBox $ take 8 . map (str . _word) $ getRandomWords gen x
+    draw = return . strWrap . unwords . take 100 . _words
+      where
+    handleEvent :: BrickEvent () () -> EventM () State ()
+    handleEvent (VtyEvent (EvKey (KChar _) [])) = return ()
+    handleEvent _ = halt
 
 main :: IO ()
 main = do
   x <- readWordBank wordBankPath
   gen <- getStdGen
   void . defaultMain app $ State {
-      _wordBank = x
-    , _stdGen = gen
+      _words = map _word $ getRandomWords gen x
+    , _typed = []
     }
