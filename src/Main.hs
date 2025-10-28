5da67ff77971e96074e44861ae39d19a21c97f60
maningreen <flibbers@protonmail.com>
2025-10-28 07:43:37 -0600

    it now has correct shading

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,6 @@
 module Main (main) where
 
-import Brick (App (..), BrickEvent (VtyEvent), EventM, attrMap, attrName, defaultMain, fg, hLimit, halt, modify, neverShowCursor, strWrap, vLimit, attrName, AttrName, str)
+import Brick (App (..), BrickEvent (VtyEvent), EventM, attrMap, attrName, defaultMain, fg, hLimit, halt, modify, neverShowCursor, strWrap, vLimit, attrName, AttrName, str, getContext, vBox, hBox, withAttr)
 import Brick.Widgets.Center (center)
 import Control.Monad (void)
 import Graphics.Vty (Event (EvKey), Key (KBS, KChar), black, defAttr, red, white)
@@ -9,7 +9,9 @@ import Types.WordBank
 import Types.WordItem (WordItem (_word))
 import qualified Data.Text as T
 import Data.Text (Text)
-import Util (textInitSafe, applyAttrToListW)
+import Util (textInitSafe, applyAttrToListW, breakChunks, merge, zipWithM)
+import qualified Data.List as Prelude
+import qualified Data.List
 
 data State = State
   { _words :: [String]
@@ -33,21 +35,18 @@ app =
             ]
     }
  where
-  draw s = typedWid : [wordsWid]
+  draw s = return wordsWid
    where
-    wordsWid = center . vLimit height . hLimit width . strWrap $ w
-     where
-      width = 100
-      height = 3
-    w = unwords . take 100 $ _words s
+    wordsWid = center . hLimit width . vLimit height . vBox . map hBox . breakChunks width . zipWithM f theString $ unpacked
+      where
+        f (Just a) (Just b) = withAttr (if a == b then attrName "typed" else attrName "wrong") $ str $ return a
+        f (Just a) (Nothing) = withAttr (attrName "default") . str $ return a
+    width = 100
+    height = 3
+    theWords = take 100 $ _words s
+    theString = unwords theWords 
     typed = _typed s
-    typedWid = applyAttrToListW (str . return . snd) f zipped
-     where
-      zipped = zip w $ T.unpack typed
-      f :: (Char, Char) -> AttrName
-      f (x, y)
-        | x == y = attrName "typed"
-        | otherwise = attrName "wrong"
+    unpacked = T.unpack typed
   handleEvent :: BrickEvent () () -> EventM () State ()
   handleEvent (VtyEvent (EvKey (KChar c) [])) = modify $ addCharToTyped c
   handleEvent (VtyEvent (EvKey KBS [])) = modify dropLastTyped
--- src/Util.hs
+++ src/Util.hs
@@ -1,8 +1,9 @@
 -- a module full of useful, pure functions
 module Util where
-import Data.Text (Text, unsnoc, empty)
+
+import Brick (AttrName, Widget, str, withAttr, (<+>))
+import Data.Text (Text, empty, unsnoc)
 import qualified Data.Text as T
-import Brick (Widget, (<+>), AttrName, withAttr, str)
 
 foldFind :: (b -> a -> (b, Bool)) -> b -> [a] -> (b, Maybe a)
 foldFind _ acc [] = (acc, Nothing)
@@ -18,25 +19,25 @@ initSafe xs = init xs
 
 textInitSafe :: Text -> Text
 textInitSafe i = case unsnoc i of
-  Nothing -> empty 
+  Nothing -> empty
   Just (t, _) -> t
 
 applyAttrToString :: (Char -> AttrName) -> Text -> (Text, [AttrName])
 applyAttrToString f s = (s, T.foldr g [] s)
-  where
-    g x a = f x : a
+ where
+  g x a = f x : a
 
 applyAttrToListW :: (a -> Widget n) -> (a -> AttrName) -> [a] -> Widget n
 applyAttrToListW _ _ [] = str ""
-applyAttrToListW g f (x:xs) = withAttr attr (g x) <+> applyAttrToListW g f xs
-  where
-    attr = f x
+applyAttrToListW g f (x : xs) = withAttr attr (g x) <+> applyAttrToListW g f xs
+ where
+  attr = f x
 
 breakChunks :: Int -> [a] -> [[a]]
 breakChunks _ [] = []
-breakChunks x xs = pre : breakChunks x post 
-  where
-    (pre, post) = splitAt (x - 1) xs
+breakChunks x xs = pre : breakChunks x post
+ where
+  (pre, post) = splitAt (x - 1) xs
 
 wrapString :: Int -> String -> [String]
 wrapString = breakChunks
@@ -47,6 +48,18 @@ wrapString = breakChunks
 -- >merge [3, 4, 5, 7] [3, 5, 19, 23, 22]
 -- [3, 4, 5, 7, 22]
 merge :: [a] -> [a] -> [a]
-merge (x:xs) (_:ys) = x : merge xs ys
+merge (x : xs) (_ : ys) = x : merge xs ys
 merge [] ys = ys
 merge xs _ = xs
+
+mapSnd ::  (b -> c) -> (a, b) -> (a, c)
+mapSnd f (a, b) = (a, f b)
+
+mapFst :: (a -> c) -> (a, b) -> (c, b)
+mapFst f (a, b) = (f a, b)
+
+zipWithM :: (Maybe a -> Maybe b -> c) -> [a] -> [b] -> [c]
+zipWithM _ [] [] = []
+zipWithM f (x : xs) (y : ys) = f (Just x) (Just y) : zipWithM f xs ys
+zipWithM f [] (y : ys) = f Nothing (Just y) : zipWithM f [] ys
+zipWithM f (x : xs) [] = f (Just x) Nothing : zipWithM f xs []
