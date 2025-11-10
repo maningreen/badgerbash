67f05630567e4f85ae6f5f98fb9fe041a85d4324
maningreen <flibbers@protonmail.com>
2025-11-09 19:20:58 -0700

    centered the UI

--- src/State.hs
+++ src/State.hs
@@ -10,9 +10,9 @@ module State (
   generateState,
 ) where
 
-import Brick (App (..), BrickEvent (AppEvent, MouseDown, VtyEvent), EventM, Location (Location), Widget, attrMap, attrName, customMain, fg, get, hBox, hLimit, halt, modify, showCursor, showFirstCursor, str, vBox, vLimit, withAttr, (<+>), (<=>), padLeftRight)
+import Brick (App (..), BrickEvent (AppEvent, MouseDown, VtyEvent), EventM, Location (Location), Widget, attrMap, attrName, customMain, fg, get, hBox, hLimit, halt, modify, padLeftRight, showCursor, showFirstCursor, str, vBox, vLimit, withAttr, (<+>), (<=>))
 import Brick.BChan (newBChan, writeBChan)
-import Brick.Widgets.Center (center)
+import Brick.Widgets.Center (center, hCenter)
 import Button (Button (..), compileButtons)
 import Control.Concurrent (forkIO, threadDelay)
 import Control.Monad (forever, void, when)
@@ -24,8 +24,8 @@ import qualified Graphics.Vty as V
 import qualified Graphics.Vty.Platform.Unix as V.Vty
 import System.Random (StdGen, newStdGen)
 import Types.WidgetID
-import qualified Types.WidgetID as Types
-import Types.WordBank (WordBank, getRandomWords, parseWordBank, readWordBank, wordBankPath)
+import qualified Types.WidgetID as WidgetID
+import Types.WordBank (WordBank, getRandomWords, readWordBank, wordBankPath)
 import Types.WordItem (WordItem (..))
 import Util (breakChunks, initSafe, log10, roundTo, snoc, trim, zipWithM)
 
@@ -98,16 +98,25 @@ generateState = do
 -- #### STATE INFO ####
 
 -- returns the time truncated
-getStateTime :: State -> Float
-getStateTime = roundTo digitCount . _seconds
+getStateTime :: State -> Time
+getStateTime = truncateTime . _seconds
+
+-- returns the time truncated
+truncateTime :: Time -> Time
+truncateTime = roundTo digitCount
  where
   digitCount = round $ log10 (1 / delay) :: Int
 
 getWpm :: State -> Float
 getWpm state
+  -- if the wpm is invalid then we simply report 0, chances are nothing's been typed
   | isNaN wpm || isInfinite wpm = 0
+  -- if the time (s) is too low then it leads to an inaccurate measurement
+  | _seconds state < tau = 0
+  -- it's a-o-k :)
   | otherwise = wpm
  where
+  tau = 5
   wpm = 60 * (fromIntegral . length . words $ _typed state) / (getStateTime state)
 
 -- #### STATE MANIPULATIONS #####
@@ -133,29 +142,27 @@ setTarget target (State a b c _) = State a b c target
 -- #### APP FUNCTIONS ####
 
 draw :: State -> [Widget WidgetID]
-draw s = buttonWidgets ++ [center $ secondsWid <+> str " " <+> wmpWid <=> wordsWid]
+draw s = [buttonWidgets, center $ secondsWid <+> str " " <+> wmpWid <=> wordsWid]
  where
-  secondsWid = str . show $ seconds
+  secondsWid = case _target s of
+    Left sec -> str . show . truncateTime $ sec - seconds
+    Right word -> str . show $ word - length typedWords
   wmpWid = str . show . roundTo (1 :: Int) $ getWpm s
 
-  buttonWidgets = [timeButtons]
+  buttonWidgets = hCenter $ (timeButtons <+> wordButtons)
    where
     wordButtons =
-      compileButtons $
-        [ Button.Button "15 Words" (Types.WidgetID.Button (SetWords 15))
-        , Button.Button "30 Words" (Types.WidgetID.Button (SetWords 30))
-        , Button.Button "45 Words" (Types.WidgetID.Button (SetWords 45))
+      hBox . map (padLeftRight 1) . compileButtons $
+        [ Button.Button "15w" (WidgetID.Button (SetWords 15))
+        , Button.Button "30w" (WidgetID.Button (SetWords 30))
+        , Button.Button "45w" (WidgetID.Button (SetWords 45))
         ]
     timeButtons =
-      let
-        compiled =
-          compileButtons $
-            [ Button.Button "15s" (Types.WidgetID.Button (SetTime 15))
-            , Button.Button "30s" (Types.WidgetID.Button (SetTime 30))
-            , Button.Button "45s" (Types.WidgetID.Button (SetTime 45))
-            ]
-       in
-        hBox $ map (padLeftRight 1) compiled
+      hBox . map (padLeftRight 1) . compileButtons $
+        [ Button.Button "15s" (WidgetID.Button (SetTime 15))
+        , Button.Button "30s" (WidgetID.Button (SetTime 30))
+        , Button.Button "45s" (WidgetID.Button (SetTime 45))
+        ]
 
   wordsWid = cursor . hLimit width . vLimit height . vBox . map hBox . breakChunks width . space $ wordsToBeDisplayed
    where
@@ -200,7 +207,7 @@ handleEvent (AppEvent Tick) = do
     else halt
 handleEvent (VtyEvent (EvKey (KChar c) [])) = modify $ addCharToTyped c
 handleEvent (VtyEvent (EvKey KBS [])) = modify dropLastTyped
-handleEvent (MouseDown (Types.WidgetID.Button (SetWords x)) _ _ _) = modify (setTarget $ Right x)
-handleEvent (MouseDown (Types.WidgetID.Button (SetTime x)) _ _ _) = modify (setTarget . Left $ fromIntegral x)
+handleEvent (MouseDown (WidgetID.Button (SetWords x)) _ _ _) = modify (setTarget $ Right x)
+handleEvent (MouseDown (WidgetID.Button (SetTime x)) _ _ _) = modify (setTarget . Left $ fromIntegral x)
 handleEvent (VtyEvent (EvKey V.KEsc _)) = halt
 handleEvent _ = return ()
